91d8b067df16e6082d8c7d60ecdae045b624c49e
Jefferson Charlles <contato@jefferdeveloper.com>
2022-03-31 13:23:09 -0300

    refacture:deploy config index 3 commit

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import {app,PORT} from '../index'
-import { Routers } from './routers';
-
-app.use(express.json())
-app.use(Routers)
-
-app.listen(PORT,()=>console.log(`👍Server running at: 🚀 http://localhost:${PORT}`))
